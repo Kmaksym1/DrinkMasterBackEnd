e79871eacb692a822aa4abdf13beb846dd3a8e05
ZabavchukBogdan <117076795+ZabavchukBogdan@users.noreply.github.com>
2023-09-22 18:39:17 +0300

    add filters

--- helpers/ctrlWrapper.js
+++ helpers/ctrlWrapper.js
@@ -1,11 +1,13 @@
-// const ctrlWrapper = (ctrl) => {
-//   const func = async (req, res, next) => {
-//     try {
-//       await ctrl(req, res, next);
-//     } catch (er) {
-//       next(er);
-//     }
-//   };
-//   return func;
-// };
-// module.exports = ctrlWrapper;
+const controllerWrapper = (ctrl) => {
+  const func = async (req, res, next) => {
+    try {
+      await ctrl(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  return func;
+};
+
+module.exports = controllerWrapper;

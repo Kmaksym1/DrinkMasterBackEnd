e585faf177c83a946c336e0b58008d335de14393
TKazhukalo <118135022+TKazhukalo@users.noreply.github.com>
2023-09-28 22:49:54 +0300

    update endpoint

--- controllers/drinks/ownCocktailRemove.js
+++ controllers/drinks/ownCocktailRemove.js
@@ -1,32 +1,23 @@
 const { recipesModel } = require('../../models/recipesModel');
 const HttpError = require('../../helpers/HttpError');
 
-const ownCocktailRemove = async(req,res)=> {
-    try {
-        const { _id } = req.user;
-         const { cocktailId } = req.params; // Припустимо, що ідентифікатор коктелю передається через параметр URL
-       // Перевірка, чи існує коктель з вказаним ідентифікатором та чи належить він користувачеві
-        const cocktail = await recipesModel.findOne({ _id: cocktailId, owner: _id });
+const ownCocktailRemove = async (req, res) => {
+    const { _id } = req.user;
+    const { id } = req.params; // Припустимо, що ідентифікатор коктелю передається через параметр URL
+    // Перевірка, чи існує коктель з вказаним ідентифікатором та чи належить він користувачеві
+    const deletedCocktail = await recipesModel.findOne(id);
 
-        if (!cocktail) {
-            throw new HttpError(404, "Коктель не знайдено або не належить користувачеві");
-        }
-
-        // Видалення коктелю
-        await cocktail.remove();
-
-        res.status(200).json({
-            code: 200,
-            message: 'Успішна операція',
-            data: recipes,
-        });
-    } catch (error) {
-     
-        res.status(error.statusCode || 500).json({
-            code: error.statusCode || 500,
-            message: error.message || 'Внутрішня помилка сервера',
-        });
+    if (!deletedCocktail) {
+        throw new HttpError(404, "Not Found");
     }
+
+    if (JSON.stringify(deletedCocktail.owner) === JSON.stringify( id )) {
+        await deletedCocktail.deleteOne();
+    } else {
+        throw HttpError(403);
+    }
+    res.json({ deletedCocktail });
 }
+
 
 module.exports = ownCocktailRemove;
--- routes/api/drinks.js
+++ routes/api/drinks.js
@@ -4,15 +4,16 @@ const { drinks } = require("../../controllers");
 const authenticate = require("../../middlewares/authenticate");
 const errorHandler = require("../../helpers/errorHandler");
 const { upload } = require("../../middlewares");
+const isValidId = require("../../middlewares/isValidId");
 
 
 router.get("/mainpage",authenticate, errorHandler(drinks.getMainPage));
-router.get("/popular", errorHandler(drinks.getPopularCocktails));
-router.get("/search", errorHandler(drinks.searchCocktails));
-router.get("/:id",  errorHandler(drinks.getRecipeById));
-router.get("/own",errorHandler(drinks.getOwnCocktails));
-router.post("/own/add",  upload.single("cocktails"),errorHandler(drinks.ownCocktailAdd));
-router.delete("/own/remove",  errorHandler(drinks.ownCocktailRemove));
-
+router.get("/popular",authenticate, errorHandler(drinks.getPopularCocktails));
+router.get("/search", authenticate,errorHandler(drinks.searchCocktails));
+router.get("/:id", authenticate,isValidId, errorHandler(drinks.getRecipeById));
+router.get("/own/all",authenticate,errorHandler(drinks.getOwnCocktails));
+router.post("/own/add",authenticate,upload.single("cocktails"),errorHandler(drinks.ownCocktailAdd));
+router.delete("/own/remove",authenticate, errorHandler(drinks.ownCocktailRemove));
+//
 
 module.exports = router;

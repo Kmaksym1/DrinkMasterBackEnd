ee98164737fe20cfb520fb8fa5206551a5334692
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-10-01 20:22:51 +0300

    changing avatar

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -3,52 +3,48 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const multer = require("multer");
 const path = require("path");
 
-cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET,
-});
-const tmpDir = path.resolve("tmp");
+const { CLOUD_NAME, CLOUD_API_KEY, CLOUD_API_SECRET } = process.env;
 
-const multerConfig = multer.diskStorage({
-  destination: tmpDir,
-  filename: (_, file, cb) => {
-    const { originalname } = file;
-    const uniquePrefix = `${Date.now()}-${Math.round(Math.random() * 1e9)}`;
-    const fileName = `${uniquePrefix}_${originalname}`;
-    cb(null, fileName);
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: CLOUD_API_KEY,
+  api_secret: CLOUD_API_SECRET,
+});
+
+const tempDir = path.resolve("temp");
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: async (req, file) => {
+    let folder;
+    let transformation = [];
+
+    switch (file.fieldname) {
+      case "avatar":
+        folder = "avatars";
+        transformation.push(
+          { crop: "crop", gravity: "face", height: 200, width: 200 },
+          { crop: "scale", width: 45 }
+        );
+        break;
+      case "drinkThumb":
+        folder = "cocktails";
+        transformation({ width: 400 });
+        break;
+      default:
+        folder = "others";
+        break;
+    }
+
+    return {
+      folder,
+      transformation,
+      allowed_formats: ["jpg", "jpeg", "png"],
+      public_id: `avatar_${req.user._id}`,
+    };
   },
 });
 
-const upload = multer({ storage: multerConfig });
-
-
-
-
-
-// const storage = new CloudinaryStorage({
-//   cloudinary: cloudinary,
-//   params: async (req, file) => {
-//     let folder;
-//     if (file.fieldname === "avatar") {
-//       folder = "avatars";
-//     } else if (file.fieldname === "cocktail") {
-//       folder = "cocktails";
-//     } else {
-//       folder = "others";
-//     }
-//     return {
-//       folder: folder,
-//       allowed_formats: ["jpg", "png"], // Adjust the allowed formats as needed
-//       public_id: file.originalname, // Use original filename as the public ID
-//       transformation: [
-//         { width: 350, height: 350 },
-//         { width: 700, height: 700 },
-//       ],
-//     };
-//   },
-// });
-
-// const upload = multer({ storage });
+const upload = multer({ storage: storage });
 
 module.exports = upload;
